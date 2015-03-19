62bcb92e23f9f0e834b86996596d332860b04600
mhuerster <mary.huerster@gmail.com>
2015-03-19 00:20:23 -0700

    formatting

--- scripts/twitter.js
+++ scripts/twitter.js
@@ -1,16 +1,16 @@
 var tweetButton = document.getElementById('tweet');
 tweetButton.onclick = function(event) {
   console.log(this);
-  var width  = 575,
+  var width = 575,
     height = 400,
-    left   = (window.outerWidth  - width)  / 2,
-    top    = (window.outerHeight - height) / 2,
-    url    = this.href,
-    opts   = 'status=1' +
-             ',width='  + width  +
-             ',height=' + height +
-             ',top='    + top    +
-             ',left='   + left;   
+    left = (window.outerWidth - width) / 2,
+    top = (window.outerHeight - height) / 2,
+    url = this.href,
+    opts = 'status=1' +
+    ',width=' + width +
+    ',height=' + height +
+    ',top=' + top +
+    ',left=' + left;
   window.open(url, 'twitter', opts);
   return false;
-  };
+};
