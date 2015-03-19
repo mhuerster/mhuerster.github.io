86beb43e226516d30509c0ee8b6d4f0283a82986
mhuerster <mary.huerster@gmail.com>
2015-03-19 00:19:23 -0700

    control window open

--- scripts/facebook.js
+++ scripts/facebook.js
@@ -1,15 +1,35 @@
 window.fbAsyncInit = function() {
   FB.init({
-    appId      : '394712660699945',
-    xfbml      : true,
-    version    : 'v2.2'
+    appId: '394712660699945',
+    xfbml: true,
+    version: 'v2.2'
   });
 };
 
-(function(d, s, id){
+function cancelEvent(event) {
+  if (event.preventDefault) {
+    event.preventDefault();
+  } else {
+    event.returnValue = false;
+  }
+}
+
+(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
-  js = d.createElement(s); js.id = id;
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
   js.src = "//connect.facebook.net/en_US/sdk.js";
   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
+}(document, 'script', 'facebook-jssdk'));
+
+var facebookButton = document.getElementById('fb');
+facebookButton.onclick = function(event) {
+  cancelEvent(event);
+  FB.ui({
+    method: 'share',
+    href: window.location.href,
+  }, function(response) {});
+};
